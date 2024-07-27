3a8acd5f09bd7028c2d80cbf1df1fde730ab49a4
dloc19 <locgit1910@gmail.com>
2024-07-27 12:12:16 +0700

    Add files via upload

---
+++ Service.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_luyen2
+{
+    internal class Service
+    {
+    }
+}
